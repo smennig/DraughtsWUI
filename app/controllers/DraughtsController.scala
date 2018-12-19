2d2d0863e23cd34bd9e9cbb560f42d1ed753555b
Simon Mennig <simon.mennig@hotmail.de>
2018-12-19 22:04:56 +0100

    render board in scala Template

---
+++ app/controllers/DraughtsController.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import de.htwg.draughts.controller._
+import de.htwg.draughts.model.{Board, BoardCreator}
+import javax.inject._
+import play.api.mvc._
+
+
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class DraughtsController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val board: Board = new BoardCreator(8).setupFields()
+  val c: GameController = new MoveController(board)
+
+  /**
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def index = Action {
+    Ok(views.html.board(c))
+  }
+
+
+  def move(oldCol: Int, oldRow: Int, newRow: Int, newCol: Int) = Action {
+
+    //FIXME switch col and row
+    println(c.move(oldRow, oldCol, newRow, newCol))
+    println((oldCol, oldRow, newCol, newRow))
+    println(c.board)
+    Ok(views.html.board(c))
+  }
+
+}

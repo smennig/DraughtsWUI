19af52ab56c86d399c27c16dd5753886b7b1c0fe
Simon Mennig <simon.mennig@hotmail.de>
2018-12-21 21:41:38 +0100

    reformat js and made fetch more concise using arrow functions

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -5,93 +5,77 @@ const fieldClass = "field";
 const kingBlack = "king-black";
 const kingWhite = "king-white";
 const highlightedClass = "highlighted";
-var lastClicked = null;
-
+let lastClicked = null;
 
 function getPiece(event) {
-    let piece = null;
-    if (event.srcElement.classList.contains(pieceClass)) {
-        const pieceElement = event.srcElement;
-        let color;
-        let isKing = false;
-        if (pieceElement.classList.contains(black)) {
-            color = black
-        } else {
-            color = white
-        }
-        if (pieceElement.classList.contains(kingBlack) || pieceElement.classList.contains(kingWhite)) {
-            isKing = true
-        }
-        piece = {
-            color: color,
-            isKing: isKing
-        }
+  let piece = null;
+  if (event.srcElement.classList.contains(pieceClass)) {
+    const pieceElement = event.srcElement;
+    let color;
+    let isKing = false;
+    if (pieceElement.classList.contains(black)) {
+      color = black;
+    } else {
+      color = white;
     }
-    return piece;
+    if (pieceElement.classList.contains(kingBlack) || pieceElement.classList.contains(kingWhite)) {
+      isKing = true;
+    }
+    piece = {
+      color: color,
+      isKing: isKing
+    };
+  }
+  return piece;
 }
 
 function getField(fieldElement, piece) {
-    let col = fieldElement.dataset.col;
-    let row = fieldElement.dataset.row
-    let field = {
-        col: col,
-        row: row,
-        piece: piece
-    }
-    return field;
+  let col = fieldElement.dataset.col;
+  let row = fieldElement.dataset.row;
+  return {
+    col: col,
+    row: row,
+    piece: piece
+  };
 }
 
 function move(oldCol, oldRow, newCol, newRow) {
-if(oldCol != null)
     fetch(`/move/${oldCol}/${oldRow}/${newCol}/${newRow}`)
-        .then(function(response) {
-            return response.text();
-        })
-        .then(function(boardHtml) {
-            let el = document.getElementById("content")
-            el.innerHTML = boardHtml;
-            registerOnClickListeners()
-        });
+      .then(response => response.text())
+      .then(boardHtml => document.getElementById("content").innerHTML = boardHtml)
+      .then(() => registerOnClickListeners())
+      .catch(e => console.log(e));
 }
 
 function registerOnClickListeners() {
-    var fields = document.getElementsByClassName(fieldClass);
-    for (var i = 0; i < fields.length; i++) {
-        fields[i].addEventListener('click', function (event) {
-            event.preventDefault();
-            if (event.currentTarget.classList.contains(fieldClass)) {
-                let fieldElement = event.currentTarget;
-                let piece = getPiece(event);
-                let field = getField(fieldElement, piece);
+  var fields = document.getElementsByClassName(fieldClass);
+  for (var i = 0; i < fields.length; i++) {
+    fields[i].addEventListener('click', function (event) {
+      event.preventDefault();
+      if (event.currentTarget.classList.contains(fieldClass)) {
+        let fieldElement = event.currentTarget;
+        let piece = getPiece(event);
+        let field = getField(fieldElement, piece);
 
-                if(field.piece != null && lastClicked == null){
-                    fieldElement.classList.add(highlightedClass);
-                    lastClicked={
-                        element: fieldElement,
-                        field:field
-                    }
-                }
-                if(lastClicked != null && field.piece == null){
-                    if(field.col && field.row) {
-                        move(lastClicked.field.col, lastClicked.field.row, field.col, field.row);
-                    }
-                    lastClicked.element.classList.remove(highlightedClass);
-                    lastClicked = null;
-                }
-            }
-        }, false);
-    }
-
-
+        if (field.piece != null && lastClicked == null) {
+          fieldElement.classList.add(highlightedClass);
+          lastClicked = {
+            element: fieldElement,
+            field: field
+          }
+        }
+        if (lastClicked != null && field.piece == null) {
+          if (field.col && field.row) {
+            move(lastClicked.field.col, lastClicked.field.row, field.col, field.row);
+          }
+          lastClicked.element.classList.remove(highlightedClass);
+          lastClicked = null;
+        }
+      }
+    }, false);
+  }
 }
 
-// without jQuery (doesn't work in older IEs)
 document.addEventListener('DOMContentLoaded', function () {
-
-    registerOnClickListeners()
-
+  registerOnClickListeners()
 }, false);
-
-
-
-
